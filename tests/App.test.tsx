c21d2e94d8d7cb676571a400a0a350031aca63d6
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2023-07-30 06:46:13 +0100

    Add basic render and router tests

---
+++ tests/App.test.tsx
@@ -0,0 +1,59 @@
+import { render, screen, getByRole } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../src/App';
+import Router from '../src/components/Router';
+import { BrowserRouter } from 'react-router-dom';
+
+describe('App renders', () => {
+    render(
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    );
+    const header = screen.getByRole('banner');
+
+    it('Renders header element', () => {
+        expect(header).toBeInTheDocument();
+    });
+
+    it('Includes a logo and navbar', () => {
+        const logo = getByRole(header, 'img');
+        const nav = getByRole(header, 'navigation');
+
+        // cast to Boolean with !!
+        const containsLogoAndNav = !!logo && !!nav;
+
+        expect(containsLogoAndNav).toBe(true);
+    });
+});
+
+describe('Routes configured correctly', () => {
+    it('Homepage renders', async () => {
+        render(<Router />);
+
+        expect(screen.getByRole('heading').textContent).toBe('Home');
+    });
+
+    it('Changes to "shop" page when shop tab clicked form homepage', async () => {
+        render(<Router />);
+
+        const user = userEvent.setup();
+
+        const shopTab = screen.getByRole('link', { name: 'Shop' });
+        await user.click(shopTab);
+
+        expect(screen.getByText(/Shop page!/i)).toBeInTheDocument();
+    });
+
+    it('Changes to "shop" then "cart"', async () => {
+        render(<Router />);
+
+        const user = userEvent.setup();
+
+        await user.click(screen.getByRole('link', { name: 'Shop' }));
+        expect(screen.getByText(/Shop page!/i)).toBeInTheDocument();
+
+        await user.click(screen.getByRole('link', { name: 'Cart' }));
+        expect(screen.getByText(/Cart page!/i)).toBeInTheDocument();
+    });
+});
--- tests/setup.ts
+++ tests/setup.ts
@@ -6,6 +6,6 @@ import matchers from '@testing-library/jest-dom/matchers';
 expect.extend(matchers);
 
 // runs a cleanup after each test case (e.g. clearing jsdom)
-afterEach((): void => {
+afterEach(() => {
     cleanup();
 });
