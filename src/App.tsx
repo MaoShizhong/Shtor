d5e6b2f8621040a097060dd04a8c96ef2405d0e8
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2023-07-31 20:19:44 +0100

    Implement item quantity change and removal in cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Router from './components/Router.tsx';
-import { FormEvent, createContext, useState } from 'react';
+import { FormEvent, ChangeEvent, createContext, useState } from 'react';
 import { Product } from './components/shop/Shop.tsx';
 
 type ID = number;
@@ -11,13 +11,19 @@ export type CartProduct = Product & {
 type Cart = {
     cart: Map<ID, CartProduct>;
     cartTotal: number;
-    addToCart: (e: FormEvent, arg1: Product, arg2: number) => void;
+    addToCart: (event: FormEvent | ChangeEvent, product: Product, quantityToAdd: number) => void;
+    removeFromCart: (
+        event: FormEvent | ChangeEvent,
+        product: Product,
+        quantityToAdd: number
+    ) => void;
 };
 
 export const CartContext = createContext<Cart>({
     cart: new Map<ID, CartProduct>(),
     cartTotal: 0,
     addToCart: () => {},
+    removeFromCart: () => {},
 });
 
 export default function App() {
@@ -40,8 +46,26 @@ export default function App() {
         setCartTotal(cartTotal + product.price * quantity);
     }
 
+    function removeFromCart(e: FormEvent, product: Product, quantity: number): void {
+        e.preventDefault();
+
+        const productInCart = cart.get(product.id);
+
+        if (productInCart) {
+            productInCart.quantity -= quantity;
+            setCart(cart.set(product.id, productInCart));
+
+            if (productInCart.quantity === 0) {
+                cart.delete(product.id);
+                setCart(cart);
+            }
+        }
+
+        setCartTotal(cartTotal - product.price * quantity);
+    }
+
     return (
-        <CartContext.Provider value={{ cart, cartTotal, addToCart }}>
+        <CartContext.Provider value={{ cart, cartTotal, addToCart, removeFromCart }}>
             <Router />
         </CartContext.Provider>
     );
--- src/components/cart/CartItem.tsx
+++ src/components/cart/CartItem.tsx
@@ -1,13 +1,29 @@
-import { CartProduct } from '../../App';
+import { ChangeEvent, useContext } from 'react';
+import { CartContext, CartProduct } from '../../App';
 import { getPriceAsCurrencyString } from '../../util';
 
 type CartItemProps = {
     item: CartProduct;
 };
 
+const quantities = [...Array(11).keys()];
+
 export function CartItem({ item }: CartItemProps) {
     const unitValue = getPriceAsCurrencyString(item.price);
     const orderValue = getPriceAsCurrencyString(item.price * item.quantity);
+
+    const { addToCart, removeFromCart } = useContext(CartContext);
+
+    function handleChange(e: ChangeEvent): void {
+        const dropdown = e.target as HTMLSelectElement;
+        const newQuantity: number = +dropdown.value;
+
+        if (newQuantity > item.quantity) {
+            addToCart(e, item, newQuantity - item.quantity);
+        } else {
+            removeFromCart(e, item, item.quantity - newQuantity);
+        }
+    }
 
     return (
         <>
@@ -16,21 +32,18 @@ export function CartItem({ item }: CartItemProps) {
                     <img className="max-w-[4rem] border border-soft" src={item.images[0]}></img>
                     <div className="w-[min(300px,50vw)]">
                         <h1 className="font-bold">{item.title}</h1>
-                        <p>
-                            Qty: {item.quantity} ({unitValue} per)
-                        </p>
+                        <div className="flex items-center gap-2">
+                            <select defaultValue={item.quantity} onChange={handleChange}>
+                                {quantities.map((quantity) => (
+                                    <option value={quantity}>{quantity}</option>
+                                ))}
+                            </select>
+                            <p>x {unitValue}</p>
+                        </div>
                     </div>
                 </div>
             </td>
-            <td className="relative flex flex-col justify-around sm:table-cell">
-                {orderValue}
-                <button
-                    className="sm:absolute text-xl transition-all duration-75 sm:-translate-y-0.5 sm:-right-12 hover:text-slate-800 hover:scale-105"
-                    aria-label="Remove item"
-                >
-                    X
-                </button>
-            </td>
+            <td className="self-center">{orderValue}</td>
         </>
     );
 }
