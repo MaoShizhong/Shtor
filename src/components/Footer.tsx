adf8e4b4f6e30d00a7fd4d57637bcdef63a04def
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2023-08-03 02:34:45 +0100

    Remove unnecessary horizontal scrollbar

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
     return (
-        <footer className="flex w-screen h-56 p-6 mt-4 text-lg text-white bg-cyan-950 justify-evenly">
+        <footer className="flex w-full h-56 p-6 mt-4 text-lg text-white bg-cyan-950 justify-evenly">
             Footer!
         </footer>
     );
