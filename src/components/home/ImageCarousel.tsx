0b7bcbd1cf383fa2d6108a4e4cf3bcb28d7664bd
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2024-01-03 22:56:34 +0000

    Fix image carousel img width in firefox

--- src/components/home/ImageCarousel.tsx
+++ src/components/home/ImageCarousel.tsx
@@ -1,25 +1,25 @@
-import { useState, useEffect } from 'react';
-import { Indicators } from './Indicators';
+import { useState } from 'react';
 import { ArrowControl } from './ArrowControl';
+import { Indicators } from './Indicators';
 
 export function ImageCarousel() {
     const images = ['living-room', 'chessboard', 'bed', 'typewriter', 'piano'];
 
     const [currentIndex, setCurrentIndex] = useState(0);
 
-    useEffect((): (() => void) => {
-        function carouselAutoplay(): void {
-            setCurrentIndex(currentIndex === images.length - 1 ? 0 : currentIndex + 1);
-        }
+    // useEffect((): (() => void) => {
+    //     function carouselAutoplay(): void {
+    //         setCurrentIndex(currentIndex === images.length - 1 ? 0 : currentIndex + 1);
+    //     }
 
-        const autoplayInterval = setInterval(carouselAutoplay, 7000);
+    //     const autoplayInterval = setInterval(carouselAutoplay, 7000);
 
-        return (): void => clearInterval(autoplayInterval);
-    }, [currentIndex, images.length]);
+    //     return (): void => clearInterval(autoplayInterval);
+    // }, [currentIndex, images.length]);
 
     return (
         <div
-            className="relative flex overflow-hidden max-h-[460px] w-full mt-6 mb-1 sm:mb-2 sm:mt-12"
+            className="relative flex overflow-hidden h-[460px] w-full mt-6 mb-1 sm:mb-2 sm:mt-12"
             aria-label="image carousel"
         >
             <ArrowControl
@@ -31,12 +31,12 @@ export function ImageCarousel() {
             {images.map((image, i): JSX.Element => {
                 return (
                     <img
-                        className="object-cover object-bottom duration-[1600ms] ease-in-out"
+                        className="object-cover object-bottom duration-[1600ms] ease-in-out w-full flex-shrink-0"
                         key={i}
                         src={`./${image}.jpg`}
                         alt={`image ${i} (${image})`}
                         style={{
-                            transform: `translate(-${currentIndex * 100}%)`,
+                            transform: `translateX(-${currentIndex * 100}%)`,
                         }}
                     />
                 );
